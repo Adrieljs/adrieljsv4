06f6a9c7bae228dc69d846c7f5e46addd99818ef
Adrieljs <71127155+Adrieljs@users.noreply.github.com>
2020-11-01 19:57:28 +0800

    e

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 function copyDiscordLink() {
-    copyer("discordlink");
-    var c = document.getElementById("copybutton");
-    c.classList = "far fa-check animated bounce";
-    c.style = "color: #13eba2";
-    setTimeout(function() {
+    this.x;
+    if (this.x === 0 || !this.x) {
+        copyer("discordlink");
+        var c = document.getElementById("copybutton");
+        c.classList = "far fa-check animated bounce";
+        c.style = "color: #13eba2";
+        this.x = 1;
+        setTimeout(function() {
         var c = document.getElementById("copybutton");
         c.classList = "fad fa-clone mouse-pointer animated bounce";
+        this.x = 0;
         c.style = '';
-    }, 5000);
+        }, 5000);
+    }
 }
